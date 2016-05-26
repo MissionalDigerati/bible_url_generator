55bcd41d56a1a265057dc7029486c81359d3fe6e
Codemis <johnathan@jpulos.com>
2016-05-26 15:41:17 -0700

    Added support for ESV.

--- js/application.js
+++ js/application.js
@@ -6,15 +6,19 @@
 var generator = new BibleUrlGenerator();
 
 jQuery(document).ready(function($) {
-  $('#submit-passage-form').submit(function() {
+  $('#submit-passage-form').submit(function(event) {
+    event.stopPropagation();
     var request = $('#passage-input').val();
     if (request) {
-      var url = generator.generate(request);
-      if (url) {
-        $('#url-value').html('').append($('<a/>').attr({href: url, target: '_blank'}).text(url));
-      } else {
+      generator.generate(request).then(function(url) {
+        if (url) {
+          $('#url-value').html('').append($('<a/>').attr({href: url, target: '_blank'}).text(url));
+        } else {
+          $('#message-holder').html('').append($('<div/>').addClass('alert alert-danger').text('Sorry, unable to generate the URL.'));
+        }
+      }, function() {
         $('#message-holder').html('').append($('<div/>').addClass('alert alert-danger').text('Sorry, unable to generate the URL.'));
-      }
+      });
     } else {
       $('#message-holder').html('').append($('<div/>').addClass('alert alert-danger').text('Please enter a valid URL.'));
     }
